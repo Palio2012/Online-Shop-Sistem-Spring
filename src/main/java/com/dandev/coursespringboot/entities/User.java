d0ca1a1b15a01e07f9b2745277ccaa5ce1146444
Palio2012 <daniasrayan.com@gmail.com>
2022-10-14 18:21:09 -0300

    H2 Database, test profile, JPA

--- src/main/java/com/dandev/coursespringboot/entities/User.java
+++ src/main/java/com/dandev/coursespringboot/entities/User.java
@@ -1,12 +1,18 @@
 package com.dandev.coursespringboot.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table (name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
