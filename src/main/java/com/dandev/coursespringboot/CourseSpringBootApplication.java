4664745b799635b04fd99fd42d0ce5cb99529505
Palio2012 <daniasrayan.com@gmail.com>
2022-10-14 13:58:35 -0300

    Project created

---
+++ src/main/java/com/dandev/coursespringboot/CourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dandev.coursespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dandev/coursespringboot/CourseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dandev.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
