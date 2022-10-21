d5cd1a79b7b07912103beceb286a0331a454bb60
Palio2012 <daniasrayan.com@gmail.com>
2022-10-21 11:19:35 -0300

    Subtotal e Total methods

--- src/main/java/com/dandev/coursespringboot/entities/Payment.java
+++ src/main/java/com/dandev/coursespringboot/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.dandev.coursespringboot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
