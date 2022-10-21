d4def14e895e487f3a5962bcf73243ea817094fa
Palio2012 <daniasrayan.com@gmail.com>
2022-10-21 12:43:17 -0300

    Exception Handling - Update

--- src/main/java/com/dandev/coursespringboot/services/UserService.java
+++ src/main/java/com/dandev/coursespringboot/services/UserService.java
@@ -4,6 +4,7 @@ import com.dandev.coursespringboot.entities.User;
 import com.dandev.coursespringboot.repositories.UserRepository;
 import com.dandev.coursespringboot.services.exceptions.DatabaseException;
 import com.dandev.coursespringboot.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,14 @@ public class UserService {
     }
 
     public User update (Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
